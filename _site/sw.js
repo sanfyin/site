be22a020796ffba486deedb28c551ff7ca043243
Sanjib Das <sanjibdas86@gmail.com>
2022-07-01 14:32:38 +0530

    Updated title

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220701.142345';
+const cacheName = 'chirpy-20220701.142752';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
