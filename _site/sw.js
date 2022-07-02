7f0f4b16886696353643abf068df46aca0d8823b
Sanjib Das <sanjibdas86@gmail.com>
2022-07-02 10:10:30 +0530

    Updated size of main content

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220701.143244';
+const cacheName = 'chirpy-20220702.100921';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
