16d0bf174e20cbcd459b36a6be2bbbe6db4713aa
sanfyin <sanjibdas86@gmail.com>
2023-11-27 23:48:58 +0530

    Added Favicons

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230717.225437';
+const cacheName = 'chirpy-20231127.234141';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
